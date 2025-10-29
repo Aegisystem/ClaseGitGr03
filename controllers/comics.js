28e33637b7056fd37623feb9d5e92d3d8fd810da
Steven Correa <steven14correa@gmail.com>
2025-10-29 18:35:28 -0500

    Endpoint conectado con API Pokemon

--- controllers/comics.js
+++ controllers/comics.js
@@ -1,7 +1,11 @@
 const comicsController = {}
+const axios = require("axios")
 
 comicsController.getComics = (req, res) => {
-    res.json( { mensaje: "Hola munde!" } )
+    const POKEMON_API = process.env.POKEMON_API
+
+    axios.get(POKEMON_API+'/pokemon')
+        .then(resp => res.json(resp.data))
 }
 
 module.exports = comicsController
